b3d4d86a27f0c5cf6480951310a779d5726bc5d4
Joonas Kemppainen <joonas.kemppainen@outlook.com>
2026-03-21 12:36:49 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,13 +2,13 @@
   'use strict';
 
   var TEAMS = [
-    {id: '35213605', key: 'Z4QDRKWRTR', name: 'Ilves/P2018/A', div: 'P9 \u00b7 Taso 1', divTitle: 'Pojat 2018 \u2013 Piirisarja Taso 1', taso: 1, divIdx: 0},
-    {id: '35213607', key: 'R8C3ZUZPBX', name: 'Ilves/P2018/B', div: 'P9 \u00b7 Taso 1', divTitle: 'Pojat 2018 \u2013 Piirisarja Taso 1', taso: 1, divIdx: 0},
-    {id: '35213608', key: '2CNSGFS7V5', name: 'Ilves/Keltainen A', div: 'P8 \u00b7 Taso 1', divTitle: 'Pojat 2018 \u2013 Piirisarja Taso 1', taso: 1, divIdx: 1},
-    {id: '35213611', key: 'K9N7PPBTBF', name: 'Ilves/Keltainen B', div: 'P8 \u00b7 Taso 1', divTitle: 'Pojat 2018 \u2013 Piirisarja Taso 1', taso: 1, divIdx: 1},
-    {id: '35213613', key: 'NKW75YPCGN', name: 'Ilves/Keltavihre\u00e4 A', div: 'P8 \u00b7 Taso 1', divTitle: 'Pojat 2018 \u2013 Piirisarja Taso 1', taso: 1, divIdx: 1},
-    {id: '35213615', key: 'SGTRRP6YUB', name: 'Ilves/Keltavihre\u00e4 B', div: 'P8 \u00b7 Taso 1', divTitle: 'Pojat 2018 \u2013 Piirisarja Taso 1', taso: 1, divIdx: 1},
-    {id: '35213617', key: 'ZV5D4CPTYF', name: 'Ilves/Vihre\u00e4 A', div: 'P8 \u00b7 Taso 2', divTitle: 'Pojat 2018 \u2013 Piirisarja Taso 2', taso: 2, divIdx: 2}
+    {id: '35213605', key: 'Z4QDRKWRTR', name: 'Ilves/P2018/A', div: 'P9', divTitle: 'Pojat 2017 \u2013 Piirisarja P9', taso: 1, divIdx: 0},
+    {id: '35213607', key: 'R8C3ZUZPBX', name: 'Ilves/P2018/B', div: 'P9', divTitle: 'Pojat 2017 \u2013 Piirisarja P9', taso: 1, divIdx: 0},
+    {id: '35213608', key: '2CNSGFS7V5', name: 'Ilves/Keltainen A', div: 'P8', divTitle: 'Pojat 2018 \u2013 Piirisarja P8', taso: 1, divIdx: 1},
+    {id: '35213611', key: 'K9N7PPBTBF', name: 'Ilves/Keltainen B', div: 'P8', divTitle: 'Pojat 2018 \u2013 Piirisarja P8', taso: 1, divIdx: 1},
+    {id: '35213613', key: 'NKW75YPCGN', name: 'Ilves/Keltavihre\u00e4 A', div: 'P8', divTitle: 'Pojat 2018 \u2013 Piirisarja P8', taso: 1, divIdx: 1},
+    {id: '35213615', key: 'SGTRRP6YUB', name: 'Ilves/Keltavihre\u00e4 B', div: 'P8', divTitle: 'Pojat 2018 \u2013 Piirisarja P8', taso: 1, divIdx: 1},
+    {id: '35213617', key: 'ZV5D4CPTYF', name: 'Ilves/Vihre\u00e4 A', div: 'P8', divTitle: 'Pojat 2018 \u2013 Piirisarja P8', taso: 2, divIdx: 1}
   ];
 
   var allResults = [];
